ffcd49d705e487c04051e67b7d670e1e67917a16
Anderson <anderson@users.noreply.github.com>
2026-01-13 01:12:08 +0000

    Ajuste de configuração feito diretamente no servidor

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        f"sqlite:///{os.path.join(BASE_DIR, 'agendamento/instance/local.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave-secreta')
+
